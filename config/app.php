990a52fb7d4a1b2229f8843e90a4aa155b470884
AKS144 <32642839+AKS144@users.noreply.github.com>
2024-06-14 09:07:17 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/photography'),
+    'url' => env('APP_URL', 'http://localhost/adminpanelmain'),
 
-    'asset_url' => env('ASSET_URL', 'http://localhost/photography/public'),
+    'asset_url' => env('ASSET_URL', 'http://localhost/adminpanelmain/public'),
 
     /*
     |--------------------------------------------------------------------------
